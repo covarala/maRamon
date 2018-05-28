b4861d35f72096e0bc673b70e5d8e92fa17e2f4f
covarala <andreobraga96@gmail.com>
2018-05-28 14:54:17 -0300

    Integração Template I

---
+++ resources/views/admin/layout/baselayout.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.layout.header')
+
+
+@yield('conteudo')
+
+@extends('admin.layout.footer')
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('admin', function (){
+    return view('admin\layout\baseLayout');
+}
+);
+
+Route::get('teste', function (){
+    return view('teste');
+});
